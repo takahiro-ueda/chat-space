5b93c5dfdfda81cd5998ac65d857752b17579f8b
takahiro-ueda <pt25ue@gmail.com>
2020-03-01 15:45:19 +0900

    インクリメンタルサーチを実装1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
 
+    def index
+        repond_to do |format|
+            format.html
+            format.json
+        end
+    end
+
     def edit
     end
 
